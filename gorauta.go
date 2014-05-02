8ca322a11c1c967dd2b09e653bb31ebc8536a25a
Aditya <aditya@netroy.in>
2014-05-02 22:57:58 +0200

    gofmt

--- gorauta.go
+++ gorauta.go
@@ -5,39 +5,39 @@ import "strings"
 type RouteMap map[string][]string
 
 type Router struct {
-  // map of all routes
-  routes RouteMap
+	// map of all routes
+	routes RouteMap
 }
 
 func (r *Router) register(path string, hosts []string) bool {
-  // TODO: support regexp based routing
-  r.routes[path] = hosts
-  return true
+	// TODO: support regexp based routing
+	r.routes[path] = hosts
+	return true
 }
 
 func (r *Router) HostFor(query string) string {
-  // Try routing by exact match
-  match := r.routes[query]
-  if match != nil && len(match) > 0 {
-    return random(match)
-  }
-  // Try to route based on prefixes
-  for path, hosts := range r.routes {
-    if strings.HasPrefix(query, path) && len(hosts) > 0 {
-      return random(hosts)
-    }
-  }
-  // Could not find a match
-  return ""
+	// Try routing by exact match
+	match := r.routes[query]
+	if match != nil && len(match) > 0 {
+		return random(match)
+	}
+	// Try to route based on prefixes
+	for path, hosts := range r.routes {
+		if strings.HasPrefix(query, path) && len(hosts) > 0 {
+			return random(hosts)
+		}
+	}
+	// Could not find a match
+	return ""
 }
 
 func NewRouter(hosts RouteMap, routes RouteMap) *Router {
-  router := new(Router)
-  router.routes = make(RouteMap)
-  for service, paths := range routes {
-    for _, path := range paths {
-      router.register(path, hosts[service])
-    }
-  }
-  return router
+	router := new(Router)
+	router.routes = make(RouteMap)
+	for service, paths := range routes {
+		for _, path := range paths {
+			router.register(path, hosts[service])
+		}
+	}
+	return router
 }
